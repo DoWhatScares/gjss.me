97d9b8da413b223670c8f4cc2ece19fad074c974
DoWhatScares <the@gjss.me>
2023-10-08 23:23:07 -0500

    New Site

--- index.js
+++
@@ -1,5 +0,0 @@
-const cookieName = "myCookie";
-const cookieValue = "myValue";
-const cookieExpires = new Date();
-cookieExpires.setMonth(cookieExpires.getMonth() + 1);
-document.cookie = `${cookieName}=${cookieValue}; SameSite=None; Secure`;
